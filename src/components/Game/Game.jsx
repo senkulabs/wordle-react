22d6cf9149286b3b53193d8a4ec36b0e27f16795
Kresna Satya <kresnasatya72@gmail.com>
2024-10-28 10:44:56 +0800

    feat: finish exercise 5

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -4,18 +4,37 @@ import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
 import { useState } from "react";
 import { checkGuess } from "../../game-helpers";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // Make debugging easier
 console.info({ answer });
 
+
+// If guess < 6 and/or user incorrect answer the guess then game still running
+// If guess < 6 and user correct answer the guess then game is over (win), guess input disabled
+// If guess = 6 and user incorrect answer the guess then game is over (lose), guess input disabled
+// If guess = 6 and user correct answer the guess then game is over (win), guess input disabled
 function Game() {
+    // running | win | lose
+    const [gameStatus, setGameStatus] = useState('running');
     const [guesses, setGuesses] = useState([]);
 
     function handleGuessInput(guess) {
         const updatedGuess = checkGuess(guess, answer);
         const updatedGuesses = [...guesses, updatedGuess];
+        if (updatedGuess.length === NUM_OF_GUESSES_ALLOWED) {
+            if (guess === answer) {
+                setGameStatus('win');
+            } else {
+                setGameStatus('lose');
+            }
+        } else {
+            if (guess === answer) {
+                setGameStatus('win');
+            }
+        }
         setGuesses(updatedGuesses);
     }
 
@@ -23,7 +42,20 @@ function Game() {
         <>
             <p>Put a game here!</p>
             <GuessResults guesses={guesses} />
-            <GuessInput handleGuessInput={handleGuessInput} />
+            <GuessInput gameStatus={gameStatus} handleGuessInput={handleGuessInput} />
+            { gameStatus === 'win' && (
+                <div className="happy banner">
+                    <p>
+                    <strong>Congratulations!</strong> Got it in {' '}
+                    <strong>{guesses.length} {guesses.length > 1 ? 'guesses' : 'guess'}</strong>.
+                    </p>
+                </div>
+            )}
+            { gameStatus === 'lose' && (
+                <div className="sad banner">
+                    <p>Sorry, the correct answer is <strong>{answer}</strong></p>
+                </div>
+            )}
         </>
     )    
 }
--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function GuessInput({ handleGuessInput }) {
+function GuessInput({ gameStatus, handleGuessInput }) {
     const [value, setValue] = useState('');
 
     function handleSubmit(event) {
@@ -13,7 +13,7 @@ function GuessInput({ handleGuessInput }) {
     <>
         <form onSubmit={handleSubmit} className="guess-input-wrapper">
             <label htmlFor="guess-input">Enter guess:</label>
-            <input required type="text" id="guess-input" minLength={5} maxLength={5} pattern="[a-zA-Z]{5}" title="5 letter word" value={value} onChange={(event) => setValue(event.target.value.toUpperCase())} />
+            <input disabled={gameStatus !== 'running'} required type="text" id="guess-input" minLength={5} maxLength={5} pattern="[a-zA-Z]{5}" title="5 letter word" value={value} onChange={(event) => setValue(event.target.value.toUpperCase())} />
         </form>
     </>);
 }
